10a12ffb3c46253f97a2a7785b4d0815e274471d
Aditya Sonel <aditya.sonel@gmail.com>
2023-08-25 13:30:08 +0530

    onboarding1 images added

--- src/projects/onboarding1/screens/Onboarding1Screen.tsx
+++ src/projects/onboarding1/screens/Onboarding1Screen.tsx
@@ -2,6 +2,8 @@ import {useNavigation} from '@react-navigation/native';
 import {NativeStackNavigationProp} from '@react-navigation/native-stack';
 import React, {FC, useState} from 'react';
 import {
+  Image,
+  ImageSourcePropType,
   StatusBar,
   StyleSheet,
   Text,
@@ -10,32 +12,38 @@ import {
 } from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
 import PagerView, {PagerViewOnPageScrollEvent} from 'react-native-pager-view';
-import Animated, {interpolateColor} from 'react-native-reanimated';
+import {interpolateColor} from 'react-native-reanimated';
 import {SafeAreaView, useSafeAreaInsets} from 'react-native-safe-area-context';
 import Icon from 'react-native-vector-icons/Ionicons';
 
-const AnimatedPagerView = Animated.createAnimatedComponent(PagerView);
+import image1 from '../../../assets/onboarding1/image1.png';
+import image2 from '../../../assets/onboarding1/image2.png';
+import image3 from '../../../assets/onboarding1/image3.png';
 
 interface IProps {}
 
-const data: {heading: string; subHeading: string; image: string}[] = [
+const data: {
+  heading: string;
+  subHeading: string;
+  image: ImageSourcePropType;
+}[] = [
   {
     heading: 'Engaing Video Lessons',
     subHeading:
       'You may want to achieve an angled gradient effect, similar to those in image editors like Photoshop',
-    image: '',
+    image: image1,
   },
   {
     heading: 'Unlimited Tests & Analysis',
     subHeading:
       'You may want to achieve an angled gradient effect, similar to those in image editors like Photoshop',
-    image: '',
+    image: image2,
   },
   {
     heading: 'Personalised Learning',
     subHeading:
       'You may want to achieve an angled gradient effect, similar to those in image editors like Photoshop',
-    image: '',
+    image: image3,
   },
 ];
 
@@ -87,18 +95,19 @@ const Onboarding1Screen: FC<IProps> = () => {
           <Icon name="arrow-back" size={32} color={'black'} />
         </TouchableOpacity>
 
-        <AnimatedPagerView
+        <PagerView
           style={[styles.pagerView]}
           initialPage={0}
           offscreenPageLimit={3}
           onPageScroll={onPageScroll}>
           {data.map((item, index) => (
             <View style={[styles.pagerViewChild]} key={`${index}`}>
+              <Image style={styles.image} source={item.image} />
               <Text style={styles.heading}>{item.heading}</Text>
               <Text style={styles.subHeading}>{item.subHeading}</Text>
             </View>
           ))}
-        </AnimatedPagerView>
+        </PagerView>
       </LinearGradient>
     </SafeAreaView>
   );
@@ -125,16 +134,23 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     padding: 12,
   },
+  image: {
+    width: 160,
+    height: 160,
+    alignSelf: 'center',
+    marginBottom: 24,
+  },
   heading: {
-    fontSize: 24,
+    fontSize: 26,
     fontFamily: 'Quicksand Bold',
+    fontWeight: 'bold',
     textAlign: 'center',
+    marginBottom: 12,
   },
   subHeading: {
     fontSize: 20,
     fontFamily: 'Quicksand Medium',
     textAlign: 'center',
-    marginTop: 12,
   },
 });
 
